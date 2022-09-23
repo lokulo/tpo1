ad7f313d0eb6ad4a11dc865dd649826e0d25f1a2
lokulo <s1410922038@gms.nutc.edu.tw>
2022-09-23 11:15:25 +0800

    2022923-2

--- BMI.js
+++ BMI.js
@@ -1,5 +1,5 @@
 var readline = require("readline-sync");
 var height = readline.question("Ur height?");
-var weightlow = Math.round((Math.pow(height/100,2))*18.5) //18.5 lowest ,
+var weightlow = Math.round((Math.pow(height/100,2))*18.5) //18.5 lowest ,Math round to rounding it
 var weighthight = parseInt((Math.pow(height/100,2))*24)   //24   hightest ,paresInt to ignore decimal places
 console.log("Your ideal weight range is="+weightlow,("kg to ") +weighthight,("kg"));
