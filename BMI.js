b2b380db6dd68f969eb9b2d311046b6c670eb8bb
lokulo <90750657+lokulo@users.noreply.github.com>
2022-09-23 10:50:20 +0800

    2022923

--- BMI.js
+++ BMI.js
@@ -1,9 +1,5 @@
-var readline = require('readline-sync');    
-
-var height = readline.question("what's your height(cm)?");
-console.log("your height:"+height);
-var weight = readline.question("what's your weight?");
-console.log("your height:"+weight);
-
-var BMI = weight/(math.pow(height/100,2));
-console.log("your BMI is:"+BMI);
+var readline = require("readline-sync");
+var height = readline.question("Ur height?");
+var weightlow = Math.round((Math.pow(height/100,2))*18.5) //18.5 lowest ,
+var weighthight = parseInt((Math.pow(height/100,2))*24)   //24   hightest ,paresInt to ignore decimal places
+console.log("Your ideal weight range is="+weightlow,("kg to ") +weighthight,("kg"));
